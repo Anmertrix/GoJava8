175be2fce5a448c7734218c86bdfc2d849f22dd6
anmertrix <bioj@i.ua>
2016-02-21 18:11:51 +0200

    v.0.0.1 First commit project.

---
+++ module1/LonelyNumber/src/LonelyNumber.java
@@ -0,0 +1,21 @@
+
+public class LonelyNumber {
+
+	public static void main(String[] args) {
+		int[] numberList = {2, 3, 2, 4, 3, 7, 2, 3, 4, 4};
+		int count;
+		for (int i = 0; i < numberList.length; i++) {
+			count = 0;
+			for (int j = 0; j < numberList.length; j++) {
+				if (numberList[i] == numberList[j]) {
+					count++;
+				}
+			}
+			if (count==1) {
+				System.out.println(numberList[i]);
+				break;
+			}
+		}
+	}
+
+}
